2bd5c5192fa7437c0811b00a8c2333dc48e9fc02
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-16 08:38:48 -0400

    feat: update entrypoint

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,14 @@
 import fastapi  # pylint: disable=w0611 # noqa: F401
-import ratelimit  # pylint: disable=w0611 # noqa: F401
 import pydantic  # pylint: disable=w0611 # noqa: F401
 import pydantic_settings  # pylint: disable=w0611 # noqa: F401
+import slowapi  # pylint: disable=w0611 # noqa: F401
 import uvicorn
 
-from src.config import LOG, ENV, APP, HEADERS
-
+from src.core.config import APP, ENV, HEADERS, LOG
+from src.core.repository import Repository
 
 if __name__ == "__main__":
+    Repository.create_table()
 
     LOG.info("\033[33mIFMS Dev Competition RESTful API was initialized 🚀")
     LOG.debug(ENV.model_dump())
@@ -18,7 +19,7 @@ if __name__ == "__main__":
         port=ENV.port,
         reload=ENV.reload,
         workers=ENV.workers,
-        # access_log=False,
+        access_log=False,
         server_header=True,
         date_header=True,
         timeout_graceful_shutdown=5,
