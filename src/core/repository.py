2294fcec5250a212ca5dc9756ae7de72de2aa8ec
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-17 05:36:26 -0400

    feat: update repository

--- src/core/repository.py
+++ src/core/repository.py
@@ -1,33 +1,58 @@
-from datetime import datetime
+from time import time
 from http import HTTPStatus
 from sqlite3 import OperationalError, connect
 
 from fastapi import HTTPException, Request
 
 from src.api.presenters import SuccessJSON
-from src.common.enums import Dynamic
-from src.common.types import Exchange
+from src.common.enums import Dynamic, Operation
+from src.common.params import ExchangeRetrieve, ExchangeUpload
 from src.core.config import LOG
-from src.utils.functions import format_report
+from src.utils.formater import (
+    format_dynamic_report,
+    format_operation_report,
+    set_operation_to_all,
+)
 
 
 class Repository:
-    __DATABASE = "database.db"
-    __CREATE_TABLE = """CREATE TABLE IF NOT EXISTS report (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        dynamic TEXT NOT NULL,
-        code TEXT NOT NULL,
-        type_in TEXT NOT NULL,
-        type_out TEXT NOT NULL,
-        timestamp TEXT NOT NULL
-    );"""
-    __INSERT = (
-        "INSERT INTO report(dynamic,code,type_in,type_out,timestamp)"
-        " VALUES(?, ?, ?, ?, ?)"
-    )
     __SELECT_DYNAMIC_REPORT = (
-        "SELECT * FROM report WHERE dynamic=? ORDER BY timestamp ASC"
+        "SELECT * FROM Report WHERE dynamic=? ORDER BY timestamp ASC"
     )
+    __INSERT = """
+        INSERT INTO Report (dynamic,code,operation,type_in,type_out,timestamp)
+        VALUES (?, ?, ?, ?, ?, ?);
+    """
+    __SELECT_OPERATION = """
+        SELECT
+            code,
+            operation,
+            COUNT(*) AS total_exchanges,
+            MIN(timestamp) AS first_timestamp,
+            MAX(timestamp) AS last_timestamp
+        FROM Report WHERE dynamic=? AND operation=? GROUP BY code;
+    """
+    __SELECT_OPERATIONS = """
+        SELECT
+            code,
+            operation,
+            COUNT(*) AS total_exchanges,
+            MIN(timestamp) AS first_timestamp,
+            MAX(timestamp) AS last_timestamp
+        FROM Report WHERE dynamic=? GROUP BY code;
+    """
+    __CREATE_TABLE = """
+        CREATE TABLE IF NOT EXISTS Report (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            dynamic TEXT NOT NULL,
+            code TEXT NOT NULL,
+            operation TEXT NOT NULL,
+            type_in TEXT NOT NULL,
+            type_out TEXT NOT NULL,
+            timestamp REAL NOT NULL
+        );
+    """
+    __DATABASE = "database.db"
 
     @classmethod
     def create_table(cls) -> None:
@@ -44,18 +69,34 @@ class Repository:
             LOG.exception(error)
 
     @classmethod
-    def add_report(cls, dynamic: Dynamic, form: Exchange) -> None:
-        values = (
+    def add_report(
+        cls,
+        dynamic: Dynamic,
+        data: ExchangeUpload | ExchangeRetrieve,
+        operation: Operation,
+    ) -> None:
+        if operation == Operation.RETRIEVE:
+            type_in, type_out = operation.value, data.type.value
+
+        elif operation == Operation.UPLOAD:
+            type_in, type_out = data.type.value, operation.value
+
+        else:
+            type_in, type_out = data.type.value, data.type.toggle.value
+
+        params = (
             dynamic.value,
-            form.code,
-            form.type.value,
-            form.type.toggle.value,
-            datetime.now().isoformat(),
+            data.code,
+            operation.value,
+            type_in,
+            type_out,
+            time(),
         )
+
         try:
             with connect(cls.__DATABASE) as connection:
                 cursor = connection.cursor()
-                cursor.execute(cls.__INSERT, values)
+                cursor.execute(cls.__INSERT, params)
                 connection.commit()
 
             LOG.info("Report added successfully")
@@ -73,19 +114,64 @@ class Repository:
                 cursor = connection.cursor()
                 cursor.execute(cls.__SELECT_DYNAMIC_REPORT, (dynamic.value,))
                 reports = cursor.fetchall()
-                connection.commit()
-
-            LOG.info(f"{dynamic.value} reports found")
-            reports = list(map(format_report, reports))
-
-            return SuccessJSON(
-                request,
-                HTTPStatus.OK,
-                f"{dynamic.value} reports found",
-                {"count": len(reports), "reports": reports},
-            )
 
         except OperationalError as error:
             raise HTTPException(
-                HTTPStatus.INTERNAL_SERVER_ERROR, "Failed"
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                f"Failed getting {dynamic.value} report",
             ) from error
+
+        LOG.info(f"{dynamic.value} reports found")
+        reports = list(map(format_dynamic_report, reports))
+
+        return SuccessJSON(
+            request,
+            HTTPStatus.OK,
+            f"{dynamic.value} reports found",
+            {
+                "dynamic": dynamic.value,
+                "count": len(reports),
+                "reports": reports,
+            },
+        )
+
+    @classmethod
+    async def get_operation_reports(
+        cls, request: Request, dynamic: Dynamic, operation: Operation
+    ) -> SuccessJSON:
+        if operation == Operation.ALL:
+            sql, params = cls.__SELECT_OPERATIONS, (dynamic.value,)
+
+        else:
+            sql = cls.__SELECT_OPERATION
+            params = (dynamic.value, operation.value)
+
+        try:
+            with connect(cls.__DATABASE) as connection:
+                cursor = connection.cursor()
+                cursor.execute(sql, params)
+                reports = cursor.fetchall()
+
+        except OperationalError as error:
+            raise HTTPException(
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                f"Failed getting {operation.value} operation report",
+            ) from error
+
+        LOG.info(f"{operation.value} operation reports found")
+
+        if operation == Operation.ALL:
+            reports = list(map(set_operation_to_all, reports))
+
+        reports = list(map(format_operation_report, reports))
+
+        return SuccessJSON(
+            request,
+            HTTPStatus.OK,
+            f"{operation.value} operation reports found",
+            {
+                "dynamic": dynamic.value,
+                "count": len(reports),
+                "reports": reports,
+            },
+        )
