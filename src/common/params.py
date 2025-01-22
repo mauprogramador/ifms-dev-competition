1081d21dbfc6bf45ba26c2ffa52faaf792db95af
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-22 18:04:06 -0400

    feat: update params, add dependencies and new dynamic form

--- src/common/params.py
+++ src/common/params.py
@@ -1,11 +1,26 @@
-from tempfile import _TemporaryFileWrapper, NamedTemporaryFile
+from hashlib import shake_256
+from http import HTTPStatus
+from os.path import exists, join
+from tempfile import NamedTemporaryFile, _TemporaryFileWrapper
 from typing import Annotated
 
-from fastapi import Form, Depends, Path, Query
-from pydantic import BaseModel, Field, field_validator
+from fastapi import (
+    Depends,
+    File,
+    Form,
+    HTTPException,
+    Path,
+    Query,
+    Request,
+    UploadFile,
+)
+from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import AfterValidator, BaseModel, Field, field_validator
 
-from src.common.enums import Dynamic, FileType, Operation
-from src.common.patterns import CODE_PATTERN
+from src.common.enums import FileType, Operation
+from src.common.patterns import CODE_PATTERN, DYNAMIC_PATTERN
+from src.core.config import ANSWER_KEY_FILENAME, IMG_DIR, OAUTH2_SCHEME, TOKEN
+from src.utils.formaters import format_code, format_dynamic
 
 
 class ExchangeRetrieve(BaseModel):
@@ -21,7 +36,7 @@ class ExchangeRetrieve(BaseModel):
     @field_validator("code", mode="after")
     @classmethod
     def code_to_upper(cls, code: str) -> str:
-        return code.upper()
+        return code.strip().upper()
 
 
 class ExchangeUpload(ExchangeRetrieve):
@@ -32,15 +47,78 @@ class ExchangeUpload(ExchangeRetrieve):
     )
 
 
+class CreateNewDynamic(BaseModel):
+    name: str = Field(
+        description="Name of the new dynamic",
+        pattern=DYNAMIC_PATTERN,
+        min_length=1,
+        max_length=50,
+        examples=["final"],
+    )
+    teams_number: int = Field(
+        description="Number of Teams to create code dirs",
+        ge=0,
+        le=200,
+        examples=[15],
+    )
+
+    @field_validator("name", mode="after")
+    @classmethod
+    def format_dynamic_name(cls, name: str) -> str:
+        return name.strip().upper().replace("-", "_").replace(" ", "_")
+
+
 async def get_temp_file():
     with NamedTemporaryFile(delete=False) as tmp_file:
         yield tmp_file
 
 
-DynamicPath = Annotated[Dynamic, Path(description="Competition dynamic")]
+async def verify_token(token: Annotated[str, Depends(OAUTH2_SCHEME)]) -> None:
+    encoded_token = shake_256(TOKEN.encode(encoding="utf-8")).hexdigest(15)
+    if token != encoded_token:
+        raise HTTPException(HTTPStatus.UNAUTHORIZED, "Invalid token")
+
+
+async def verify_start(request: Request) -> None:
+    if not request.app.state.start:
+        raise HTTPException(
+            HTTPStatus.LOCKED, "Request sending has not started yet"
+        )
+
+    answer_key_path = join(IMG_DIR, ANSWER_KEY_FILENAME)
+
+    if not exists(answer_key_path):
+        raise HTTPException(HTTPStatus.NOT_FOUND, "Answer-Key image not found")
+
+
+Oauth2Token = Depends(verify_token)
+
+HasStarted = Depends(verify_start)
+
+LoginForm = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+DynamicPath = Annotated[
+    Annotated[str, AfterValidator(format_dynamic)],
+    Path(
+        description="Competition dynamic",
+        min_length=1,
+        max_length=50,
+        pattern=DYNAMIC_PATTERN,
+    ),
+]
+
+NewDynamicForm = Annotated[
+    CreateNewDynamic,
+    Form(description="Add a new dynamic and its teams code dirs"),
+]
+
+AnswerKeyFile = Annotated[
+    UploadFile,
+    File(media_type="multipart/form-data", description="Answer Key image"),
+]
 
 CodePath = Annotated[
-    str,
+    Annotated[str, AfterValidator(format_code)],
     Path(
         description="Directory code (4 letters)",
         min_length=4,
