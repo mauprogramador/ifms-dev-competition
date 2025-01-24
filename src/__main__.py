03e762cf2380c66b0b73f69dc6f7bbf3aaf663e0
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-24 00:06:34 -0400

    fix: create dirs before run the app

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,5 @@
+from os import makedirs
+
 import fastapi  # pylint: disable=w0611 # noqa: F401
 import itsdangerous  # pylint: disable=w0611 # noqa: F401
 import PIL  # pylint: disable=w0611 # noqa: F401
@@ -9,9 +11,12 @@ import slowapi  # pylint: disable=w0611 # noqa: F401
 import uvicorn
 import webdriver_manager  # pylint: disable=w0611 # noqa: F401
 
-from src.core.config import APP, ENV, HEADERS, LOG
+from src.core.config import APP, ENV, HEADERS, LOG, IMG_DIR, WEB_DIR
 
 if __name__ == "__main__":
+
+    makedirs(WEB_DIR, exist_ok=True)
+    makedirs(IMG_DIR, exist_ok=True)
 
     LOG.info("\033[33mIFMS Dev Competition RESTful API was initialized 🚀")
     LOG.debug(ENV.model_dump())
--- src/api/lifespan.py
+++ src/api/lifespan.py
@@ -1,17 +1,14 @@
 from contextlib import asynccontextmanager
-from os import makedirs
+
 
 from fastapi import FastAPI
 
-from src.core.config import IMG_DIR, WEB_DIR, WEB_DRIVER
+from src.core.config import WEB_DRIVER
 from src.core.repository import Repository
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-
-    makedirs(WEB_DIR, exist_ok=True)
-    makedirs(IMG_DIR, exist_ok=True)
 
     Repository.create_table()
 
