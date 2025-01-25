1f8af55e18b151e3a64f6d579158eb9256011766
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-25 09:52:26 -0400

    feat: remove clean tables method

--- src/repository/base_repository.py
+++ src/repository/base_repository.py
@@ -1,7 +1,4 @@
-from http import HTTPStatus
 from sqlite3 import OperationalError, connect
-
-from fastapi import HTTPException
 
 from src.core.config import ENV, LOG
 from src.repository import queries
@@ -24,20 +21,3 @@ class BaseRepository:
         except OperationalError as error:
             LOG.exception(error)
             raise OperationalError("Failed to create tables") from error
-
-    @classmethod
-    def clean_tables(cls) -> None:
-        try:
-            with connect(cls.__DATABASE) as connection:
-                cursor = connection.cursor()
-                cursor.execute(queries.DELETE_ALL_REPORTS)
-                cursor.execute(queries.DELETE_ALL_DYNAMICS)
-                connection.commit()
-
-            LOG.info("Tables cleaned successfully")
-
-        except OperationalError as error:
-            LOG.exception(error)
-            raise HTTPException(
-                HTTPStatus.INTERNAL_SERVER_ERROR, "Failed to clean tables"
-            ) from error
