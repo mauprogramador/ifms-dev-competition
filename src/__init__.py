65132d6b12a90e681946a733c045df16b601f159
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-26 00:46:27 -0400

    chore: update version to 0.5.4

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ IFMS Dev Competition
 RESTful API for managing the IFMS Development Competition
 """
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 __license__ = "MIT"
 
 __author__ = "mauprogramador"
