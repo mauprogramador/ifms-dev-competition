9851b836a41d1de735c13c45202553f9e42de6ba
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-28 18:10:49 -0400

    feat: move reports routes and cases

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,10 +2,12 @@ from .admin import router as admin_router
 from .code_dirs import router as code_dirs_router
 from .dynamics import router as dynamics_router
 from .files import router as files_router
+from .reports import router as reports_router
 
 __all__ = [
     "admin_router",
     "dynamics_router",
     "code_dirs_router",
     "files_router",
+    "reports_router",
 ]
