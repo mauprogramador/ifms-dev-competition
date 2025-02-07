d2154a26216b72d4332a8b9faacf1b4f18f721ec
mauprogramador <sir.silvabmauricio@gmail.com>
2025-02-07 02:14:52 -0400

    build: removes pillow

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,6 @@ import cv2  # pylint: disable=w0611 # noqa: F401
 import fastapi  # pylint: disable=w0611 # noqa: F401
 import itsdangerous  # pylint: disable=w0611 # noqa: F401
 import numpy  # pylint: disable=w0611 # noqa: F401
-import PIL  # pylint: disable=w0611 # noqa: F401
 import pydantic  # pylint: disable=w0611 # noqa: F401
 import pydantic_settings  # pylint: disable=w0611 # noqa: F401
 import selenium  # pylint: disable=w0611 # noqa: F401
