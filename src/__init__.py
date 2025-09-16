4ea8ad3dec2c2440875e855acf49afa2e5a7d356
mauprogramador <sir.silvabmauricio@gmail.com>
2025-09-16 10:46:10 -0400

    style: add comments

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# Copyright (C) 2024 All rights reserved.
+# Federal Institute of Mato Grosso do Sul (IFMS - Campus Três Lagoas)
+# - Homepage: https://www.ifms.edu.br/campi/campus-tres-lagoas
+# Maurício da Silva Batista (@mauprogramador)
+# - GitHub: https://github.com/mauprogramador
+# Source Code: https://github.com/mauprogramador/ifms-dev-competition
 
 """
 IFMS Dev Competition
@@ -6,7 +12,7 @@ IFMS Dev Competition
 RESTful API for managing the IFMS Development Competition
 """
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 __license__ = "MIT"
 
 __author__ = "mauprogramador"
