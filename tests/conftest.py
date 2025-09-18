f717590a470f4ed74de11eb5336b1a108380eee2
mauprogramador <sir.silvabmauricio@gmail.com>
2025-09-17 20:12:24 -0400

    test: run file server process to load pages for playwright

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
+from http import HTTPStatus
 from os import remove
 from shutil import rmtree
+from subprocess import DEVNULL, Popen
+from time import sleep
 from urllib.parse import urljoin
 
-from httpx import ASGITransport, AsyncClient
+from httpx import ASGITransport, AsyncClient, get
 from pytest import fixture
 from pytest_asyncio import fixture as async_fixture
 from uvloop import EventLoopPolicy, install
 
 from src.api.main import app
-from src.core.config import IMG_DIR, ROUTE_PREFIX, WEB_DIR
+from src.core.config import IMG_DIR, LOG, ROUTE_PREFIX, WEB_DIR
 from src.core.screenshot_service import ScreenshotService
 from src.repository.base_repository import BaseRepository
 from tests.mocks import DATABASE, DYNAMIC_IMG_PATH, DYNAMIC_WEB_PATH
@@ -52,9 +55,44 @@ def lifespan():
 
 @async_fixture(scope="session", loop_scope="session", name="screenshot")
 async def load_screenshot_service():
+    process = Popen(
+        [
+            "python3",
+            "-m",
+            "http.server",
+            str(CLIENT[1]),
+            "--bind",
+            CLIENT[0],
+            "--directory",
+            ".",
+        ],
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+    )
+    assert process.poll() is None
+
+    LOG.info(
+        "\033[33mStarted HTTP file server process"
+        f" [\033[36m{process.pid}\033[33m]"
+    )
+    sleep(3)
+
+    try:
+        res = get(f"{BASE_URL}/{str(WEB_DIR)}/")
+        assert res.status_code == HTTPStatus.OK
+    except Exception as exc:
+        process.terminate()
+        process.wait()
+        raise RuntimeError("HTTP file server failed to start") from exc
+
+    ScreenshotService.BASE_URL = BASE_URL
     await ScreenshotService.initialize()
+
     yield
+
     await ScreenshotService.cleanup()
+    process.terminate()
+    process.wait()
 
 
 @async_fixture(name="client")
